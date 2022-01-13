a3c462908446337d389567dc3a86b4b2576c19ad
Tr1ll10ns <elias@postfamilydigital.net>
2022-01-12 20:39:36 -0700

    Config

---
+++ RapidReact/src/main/java/frc/robot/config/Config.java
@@ -0,0 +1,62 @@
+package frc.robot.config;
+
+public class Config {
+
+    // List of subsystem names:
+    // Autonomous
+    // Climber
+    // Drive
+    // Intake
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Subsystem Enablers
+    public boolean enableAutonomousSubsystem = true;
+    public boolean enableClimberSubsystem = true;
+    public boolean enableDriveSubsystem = true;
+    public boolean enableIntakeSubsystem = true;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Motor IDs
+
+    // Autonomous Subsystem
+
+    // Climber Subsystem
+
+    // Drive Subsystem
+
+    // Intake Subsystem
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Subsystem Configs
+
+    // Autonomous Config
+    public class AutonomousConfig {
+        public AutonomousConfig() {
+
+        }
+    }
+
+    // Climber Config
+    public class ClimberConfig {
+        public ClimberConfig() {
+
+        }
+    }
+
+    // Drive Config
+    public class DriveConfig {
+        public DriveConfig() {
+
+        }
+    }
+
+    // Intake Config
+    public class IntakeConfig {
+        public IntakeConfig() {
+
+        }
+    }
+
+    public Config() {
+    }
+}
---
+++ RapidReact/src/main/java/frc/robot/config/urations/JuniorConfig.java
@@ -0,0 +1,27 @@
+package frc.robot.config.urations;
+
+import frc.robot.config.Config;
+
+public class JuniorConfig extends Config {
+
+    public JuniorConfig() {
+        super();
+        //////////////////////////////////////////////////////////////////////////////
+        // Subsystem Enablers
+        enableAutonomousSubsystem = false;
+        enableClimberSubsystem = false;
+        enableDriveSubsystem = false;
+        enableIntakeSubsystem = false;
+
+        //////////////////////////////////////////////////////////////////////////////
+        // Motor IDs
+
+        // Autonomous Subsystem
+
+        // Climber Subsystem
+
+        // Drive Subsystem
+
+        // Intake Subsystem
+    }
+}
