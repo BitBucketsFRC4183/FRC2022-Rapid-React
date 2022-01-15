46bca87457643a27209a49ec4d9994d1572c1d06
Saptarshi Mallick <31602102+Calculus-Master@users.noreply.github.com>
2022-01-15 16:54:16 -0700

    BitBucketsSubsystem now accepts a Config object (#10)
    * Added Config object as a field in BitBucketsSubsystem
    
    * Added a config object to Robot.java
    
    * Added Config object to DriveSubsystem constructor

--- RapidReact/src/main/java/frc/robot/subsystem/BitBucketsSubsystem.java
+++ RapidReact/src/main/java/frc/robot/subsystem/BitBucketsSubsystem.java
@@ -4,17 +4,21 @@ import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.Config;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BitBucketsSubsystem extends SubsystemBase
 {
+    protected final Config config;
     private final List<BaseTalon> motors;
 
-    protected BitBucketsSubsystem()
+    protected BitBucketsSubsystem(Config config)
     {
         this.setName(this.getClass().getSimpleName());
+
+        this.config = config;
 
         this.motors = new ArrayList<>();
         this.addMotorsToList();
