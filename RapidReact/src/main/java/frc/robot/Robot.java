71e213bbda69c8c00e60dac01ce4a7aa7f81ab3c
Saptarshi Mallick <sapmallick@gmail.com>
2022-01-15 18:03:26 -0700

    Removed the SendableChooser stuff from Robot.java

--- RapidReact/src/main/java/frc/robot/Robot.java
+++ RapidReact/src/main/java/frc/robot/Robot.java
@@ -5,8 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.config.Config;
@@ -24,11 +22,6 @@ import java.util.List;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
   private Buttons buttons;
   private Config config;
 
@@ -45,10 +38,6 @@ public class Robot extends TimedRobot {
 
     this.config = new Config();
     this.buttons = new Buttons();
-
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
 
     //Add Subsystems Here
     this.robotSubsystems.add(drivetrainSubsystem = new DrivetrainSubsystem(this.config));
@@ -94,23 +83,13 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+
   }
 
   /** This function is called once when teleop is enabled. */
