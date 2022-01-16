88b0003182d4390858869d5183fb0665c63c6ffd
Saptarshi Mallick <31602102+Calculus-Master@users.noreply.github.com>
2022-01-16 16:56:26 -0700

    Added blank IntakeSubsystem and ShooterSubsystem (#18)

---
+++ RapidReact/src/main/java/frc/robot/subsystem/IntakeSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystem;
+
+import frc.robot.config.Config;
+
+public class IntakeSubsystem extends BitBucketsSubsystem
+{
+    public IntakeSubsystem(Config config)
+    {
+        super(config);
+    }
+
+    @Override
+    public void init()
+    {
+
+    }
+
+    @Override
+    public void periodic()
+    {
+
+    }
+
+    @Override
+    public void disable()
+    {
+
+    }
+}
