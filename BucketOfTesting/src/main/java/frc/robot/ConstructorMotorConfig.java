74c2643b552e2683de30a9893a3d7a3742471e02
superyuuki <yuuki@superyuuki.com>
2022-02-13 14:11:02 -0700

    testing

---
+++ BucketOfTesting/src/main/java/frc/robot/ConstructorMotorConfig.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import frc.robot.config.MotorConfig;
+
+public class ConstructorMotorConfig extends MotorConfig {
+
+    public ConstructorMotorConfig(int id) {
+        this.id = id;
+    }
+}
