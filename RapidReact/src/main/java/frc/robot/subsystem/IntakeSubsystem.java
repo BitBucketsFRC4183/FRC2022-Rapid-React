7d65625c63272a01f16f6c8f9d6cf8dce5b95cfc
Ibro-cyber <71227338+Ibro-cyber@users.noreply.github.com>
2022-02-13 14:29:26 -0700

    configured solenoids for smartdashboard (#93)
    * Intake Subsystem motors initialize and intakeIn and Out functions made
    
    * Changed sim buttons
    
    * Added logger() code
    
    * Edited intake subsystem
    
    Added DoubleSolenoid and also changed toggle function
    
    * Additional logger() code
    
    * Wired up toggle button for intake
    
    * added shuffleboard config to intake module
    
    * added shuffleboard config to shooter module
    
    * filled in disable() function
    
    * added some comments to intake subsystem
    
    * filled in disable() for climber
    
    * fixed shooter (it's fixed now)
    
    * fixed feeder voltage
    
    * configured solenoids for smartdashboard
    
    Co-authored-by: Nitish Bhamidipati <nitishbhamidipati@gmail.com>
    Co-authored-by: cpostbitbuckets <43726877+cpostbitbuckets@users.noreply.github.com>

--- RapidReact/src/main/java/frc/robot/subsystem/IntakeSubsystem.java
+++ RapidReact/src/main/java/frc/robot/subsystem/IntakeSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import frc.robot.Robot;
 import frc.robot.config.Config;
 import frc.robot.log.*;
 
@@ -30,9 +31,13 @@ public class IntakeSubsystem extends BitBucketsSubsystem {
     ballManagement = new WPI_TalonSRX(Config.ballManagementMotor_ID);
     intake = new WPI_TalonSRX(Config.intakeMotor_ID);
     if (config.enablePneumatics) {
+      if (Robot.isSimulation()) {
+        intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, config.intakeSolenoid_ID1, config.intakeSolenoid_ID2);
+      } else {
       intakeSolenoid =
         new DoubleSolenoid(PneumaticsModuleType.REVPH, config.intakeSolenoid_ID1, config.intakeSolenoid_ID2);
     }
+  }
     //shows the speed of intake on the smart dashboard
   }
 
