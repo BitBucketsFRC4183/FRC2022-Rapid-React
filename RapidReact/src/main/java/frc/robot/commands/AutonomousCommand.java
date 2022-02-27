57f08ed5b2b1d9c0b9294b48fdb89a3cac62f269
Saptarshi Mallick <sapmallick@gmail.com>
2022-02-27 15:22:04 -0700

    Updated the Autonomous shoot routine

--- RapidReact/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ RapidReact/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.log.BucketLog;
 import frc.robot.log.LogLevel;
 import frc.robot.log.Loggable;
@@ -32,8 +33,9 @@ public class AutonomousCommand extends SequentialCommandGroup
     public AutonomousCommand executeShootPreload()
     {
         this.addCommands(new InstantCommand(() -> this.shooter.shootTop())
-                .andThen(new WaitCommand(5)
-                        .andThen(() -> this.shooter.stopShoot())));
+                .andThen(new WaitUntilCommand(() -> this.shooter.isUpToSpeed())
+                        .andThen(new WaitCommand(5)
+                                .andThen(() -> this.shooter.stopShoot()))));
         return this;
     }
 
