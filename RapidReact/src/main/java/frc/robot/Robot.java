469ff81fc13ce8ff8cf6a10f16c4e49961d8d38e
Scott Song <ScottSong2020@gmail.com>
2022-03-23 20:42:53 -0700

    redo

--- RapidReact/src/main/java/frc/robot/Robot.java
+++ RapidReact/src/main/java/frc/robot/Robot.java
@@ -312,7 +312,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    drivetrainSubsystem.stop();
+  }
 
   @Override
   public void simulationPeriodic() {
