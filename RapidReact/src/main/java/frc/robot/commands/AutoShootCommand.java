251f452b89a7ef0d43d2d3e4e25781f8b99773f2
superyuuki <yuuki@superyuuki.com>
2022-03-23 19:02:55 -0700

    auto

--- RapidReact/src/main/java/frc/robot/commands/AutoShootCommand.java
+++ RapidReact/src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -5,11 +5,13 @@ import frc.robot.subsystem.IntakeSubsystem;
 import frc.robot.subsystem.RGBSubsystem;
 import frc.robot.subsystem.ShooterSubsystem;
 
+import java.util.function.BooleanSupplier;
+
 public class AutoShootCommand extends SequentialCommandGroup
 {
-    private ShooterSubsystem shooter;
-    private IntakeSubsystem intake;
-    private RGBSubsystem rgb;
+    private final ShooterSubsystem shooter;
+    private final IntakeSubsystem intake;
+    private final RGBSubsystem rgb;
 
     private boolean top;
 
@@ -34,7 +36,10 @@ public class AutoShootCommand extends SequentialCommandGroup
 
     private Command getSingleBallShootCommand()
     {
-        return new InstantCommand(this.top ? () -> this.shooter.spinUpTop() : () -> this.shooter.shootLow()) //Activate shooter
+
+        Runnable shoot = top ? shooter::spinUpTop : shooter::shootLow;
+
+        return new InstantCommand(shoot) //Activate shooter
                 .andThen(new WaitCommand(0.3) //new WaitUntilCommand(() -> this.shooter.isUpToSpeed()) //Wait until shooter is up to speed
                         .andThen(() -> this.rgb.autoShootingSingle()) //Very important RGB
                         .andThen(this::enableFeedersBMS) //Feed ball
@@ -44,13 +49,17 @@ public class AutoShootCommand extends SequentialCommandGroup
 
     private Command getDoubleBallShootCommand()
     {
-        return new InstantCommand(this.top ? () -> this.shooter.spinUpTop() : () -> this.shooter.shootLow()) //Activate shooter
-                .andThen(new WaitUntilCommand(() -> this.shooter.isUpToHighSpeed()) //Wait until shooter is up to speed
+
+        Runnable shoot = top ? shooter::spinUpTop : shooter::shootLow;
+        BooleanSupplier wait = top ? shooter::isUpToHighSpeed : shooter::isUpToLowSpeed;
+
+        return new InstantCommand(shoot) //Activate shooter
+                .andThen(new WaitUntilCommand(wait) //Wait until shooter is up to speed
                         .andThen(() -> this.rgb.autoShootingDouble()) //Very important RGB
                         .andThen(this::enableFeedersBMS) //Activate feeders (and BMS just in case)
                         .andThen(new WaitCommand(0.2) //Wait, then turn off feeders (how long ball #1 takes to shoot)
                                 .andThen(this::disableFeedersBMS)
-                                .andThen(new WaitUntilCommand(() -> this.shooter.isUpToHighSpeed()) //Wait for shooter to get up to speed again (pause between shots, shooting ball #2)
+                                .andThen(new WaitUntilCommand(wait) //Wait for shooter to get up to speed again (pause between shots, shooting ball #2)
                                         .andThen(new WaitCommand(0.7) //Extra pause between shots
                                                 .andThen(this::enableFeedersBMS)
                                                 .andThen(new WaitCommand(0.5) //Wait, then turn off everything (how long it takes ball #2 to feed and shoot)
