b6d807e66a29f5e8d2ad085e9148e122d301e627
Nitish Bhamidipati <nitishbhamidipati@gmail.com>
2022-09-04 15:49:18 -0700

    Fix silly mistakes w/ buttons :)

--- RapidReact/src/main/java/frc/robot/Buttons.java
+++ RapidReact/src/main/java/frc/robot/Buttons.java
@@ -14,16 +14,11 @@ public class Buttons {
   int swerveForward = Xbox.LEFT_STICK_Y;
   int swerveStrafe = Xbox.LEFT_STICK_X;
   int swerveRotation = Xbox.RIGHT_STICK_X;
-  //JoystickButton driverEnableClimber = new JoystickButton(driverControl, PS4.PS4);
+
   JoystickButton resetOdometry = new JoystickButton(driverControl, Xbox.OPTIONS);
-  JoystickButton slowDrive = new JoystickButton(driverControl, Xbox.R2);
-
-  //JoystickButton slowDrive = new JoystickButton(driverControl, PS4.R2);
-
   int lt = 2;
   int rt = 3;
   Button slowDrive = new Button(() -> driverControl.getRawAxis(lt) > 0.1);
-
   //Operator
   Joystick operatorControl = new Joystick(1);
 
@@ -32,7 +27,6 @@ public class Buttons {
 
   JoystickButton hubSpinUp = new JoystickButton(operatorControl, Xbox.R1);
 
-  //JoystickButton lowShoot = new JoystickButton(operatorControl, PS4.R2);
   JoystickButton toggleElevator = new JoystickButton(operatorControl, Xbox.L1);
   JoystickButton feedInFire = new JoystickButton(operatorControl, Xbox.L2);
   JoystickButton autoShoot = new JoystickButton(operatorControl, Xbox.SQUARE);
@@ -41,13 +35,6 @@ public class Buttons {
   JoystickButton toggleIntake = new JoystickButton(operatorControl, Xbox.CIRCLE);
   JoystickButton intake = new JoystickButton(operatorControl, Xbox.TRIANGLE);
   JoystickButton outtake = new JoystickButton(operatorControl, Xbox.CROSS);
-
-  //JoystickButton operatorEnableClimber = new JoystickButton(operatorControl, PS4.PS4);
-  //JoystickButton climbAuto = new JoystickButton(operatorControl, PS4.TRACKPAD);
-  //JoystickButton resetClimbStuff = new JoystickButton(operatorControl, PS4.OPTIONS);
-
-  JoystickButton autoClimbStopLeft = new JoystickButton(operatorControl, Xbox.L2);
-  JoystickButton autoClimbStopRight = new JoystickButton(operatorControl, Xbox.R2);
 
   JoystickButton rgb = new JoystickButton(operatorControl, Xbox.SHARE);
 
